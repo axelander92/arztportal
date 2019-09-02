c4fd88bf93956a60812ae89ebf2b3332f0717ce4
Christoph Bähr <christoph.baehr@live.com>
2019-09-02 18:46:49 +0200

    fetch is better than httpClient

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -8,22 +8,31 @@ const httpOptions = {
   })
 };
 
-const credentials = {
-  "userid" : "nlh28466",
-  "password" : "z1m9jsl6qcrgvt@v"
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  constructor(private http: HttpClient) { }
+	private endpoint: string;
+
+  constructor(private http: HttpClient) { 
+    this.endpoint = 'https://dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net:8443/dbapi/v3/';
+  }
 
 
   callRestService() {
-    console.log("Service!")
-    this.http.post('https://dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net:8443/dbapi/v3/auth/tokens', "{\"userid\":\"nlh28466\",\"password\":\"z1m9jsl6qcrgvt@v\"}", httpOptions ).subscribe((data) => console.log(data));;
+    console.log("Service!");
+    let payload = {
+    	userid:"nlh28466",
+    	password: "z1m9jsl6qcrgvt@v"
+     };
+    fetch('https://dashdb-txn-sbox-yp-lon02-01.services.eu-gb.bluemix.net:8443/dbapi/v3/auth/tokens', {
+      body: JSON.stringify(payload),
+      method: 'POST',
+    }).then( response => response.json()).then( value => {
+      console.log(value);
+    });
   }
 
 }

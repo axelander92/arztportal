592cbc915d4136ab1dc5928791ac84b71de43c8a
Alexander Pawlowski <ute0182@dakentw007200.de>
2019-09-03 08:43:55 +0200

    patient-detail uses correct name

--- src/app/patient-detail/patient-detail.component.ts
+++ src/app/patient-detail/patient-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { PatientService } from '../patient.service';
 import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,13 +10,19 @@ import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
 export class PatientDetailComponent implements OnInit {
 
 	private patientAddress: string;
+  public patient: {};
 
-  constructor(private activatedRoute: ActivatedRoute) { 
+
+  constructor(private activatedRoute: ActivatedRoute, private patientService: PatientService, private ngZone: NgZone) {
 
     this.patientAddress = activatedRoute.snapshot.params['address'];
   }
 
   ngOnInit() {
+    this.patientService.getWSResolvePatient(this.patientAddress).subscribe( obj => {
+      console.log(obj);
+      this.ngZone.run( () => {this.patient = obj;});
+    });
   }
 
 }
